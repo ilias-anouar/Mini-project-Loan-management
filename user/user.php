28b40d92e8d7a946c62ba8e69ebab6ccebb54fbc
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-03-07 16:50:52 +0100

    ADD

--- user/user.php
+++ user/user.php
@@ -5,9 +5,9 @@ include "../head.php";
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-warning-subtle">
             <div class="container-fluid">
-                <a class="navbar-brand"><span id="brand">Choice</span> library</a>
+                <a class="navbar-brand text-warning"><span id="brand">Choice</span> library</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
                     aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -31,6 +31,43 @@ include "../head.php";
             </div>
         </nav>
     </header>
+    <main>
+        <section class="d-flex justify-content-center mt-5">
+            <form method="post">
+                <div class="row g-3 align-items-center border border-secondary border-2 rounded pb-3 fs-5 px-3">
+                    <div class="col-auto">
+                        <label for="inputPassword6" class="col-form-label">Type</label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="select" id="inputPassword6" class="form-control"
+                            aria-describedby="passwordHelpInline">
+                    </div>
+                    <div class="col-auto">
+                        <label for="inputPassword6" class="col-form-label">State</label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="select" id="inputPassword6" class="form-control"
+                            aria-describedby="passwordHelpInline">
+                    </div>
+                    <div class="col-auto">
+                        <label for="inputPassword6" class="col-form-label">Title</label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="select" id="inputPassword6" class="form-control"
+                            aria-describedby="passwordHelpInline">
+                    </div>
+                    <div class="col-auto">
+                        <input type="submit" name="search" value="Search" class="btn"
+                            aria-describedby="passwordHelpInline">
+                    </div>
+                </div>
+            </form>
+        </section>
+        <section>
+            <h2>New books</h2><br>
+            <hr>
+        </section>
+    </main>
 </body>
 
 </html>
